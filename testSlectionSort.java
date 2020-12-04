4957362074b14755cf8db722a477e0e733f75408
Aleks <asavich@umich.edu>
2020-12-03 19:38:29 -0800

    Positive and Negative methods added

---
+++ testSlectionSort.java
@@ -0,0 +1,65 @@
+import static org.junit.Assert.*;
+import org.junit.Assert;import org.junit.Test;
+import org.junit.Test;
+
+public class LargestTest  {
+	private Largest temp1;
+	@Test
+	public void test() {
+		testPositive();
+		testNegative();
+		testMixes();
+		testDuplicates();
+
+		}
+	public void testPositive(){
+		int[] arr = new int[5];
+		arr[0] = 8;
+		arr[1] = 9;
+		arr[2] = 7;
+		arr[3] = 10;
+		arr[4] = 2;
+		
+		
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = 2;
+		Sortedarr[1] = 7;
+		Sortedarr[2] = 8;
+		Sortedarr[3] = 9;
+		Sortedarr[4] = 10;
+		
+		Largest temp1 = new Largest();
+		int[] maxValue = temp1.largest(arr);
+		assertArrayEquals(Sortedarr,arr);
+		  
+		}
+
+	
+	public void testNegative(){        
+		int[] arr = new int[5];
+		arr[0] = -8;
+		arr[1] = -9;
+		arr[2] = -7;
+		arr[3] = -10;
+		arr[4] = -2;
+		
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = -2;
+		Sortedarr[1] = -7;
+		Sortedarr[2] = -8;
+		Sortedarr[3] = -9;
+		Sortedarr[4] = -10;
+		
+		Largest temp1 = new Largest();
+		int[] maxValue = temp1.largest(arr);
+		assert maxValue == -2;
+		}
+	
+	public void testMixes(){        
+		
+		}
+	
+	public void testDuplicates(){
+		
+	}
+}
