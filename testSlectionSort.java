cc198572019a022a7004573b2347f5311bcf7cc4
Aleks <asavich@umich.edu>
2020-12-03 19:52:20 -0800

    final commit

--- testSlectionSort.java
+++ testSlectionSort.java
@@ -1,5 +1,4 @@
-
-package Lab10.java;
+package SelectionSort.java;
 
 import static org.junit.Assert.*;
 import org.junit.Assert;import org.junit.Test;
@@ -23,16 +22,18 @@ public class testSelectionSort  {
 		arr[3] = 10;
 		arr[4] = 2;
 		
+		
 		int[] Sortedarr = new int[5];
 		Sortedarr[0] = 2;
 		Sortedarr[1] = 7;
 		Sortedarr[2] = 8;
 		Sortedarr[3] = 9;
 		Sortedarr[4] = 10;
-
+		
 		SelectionSort temp1 = new SelectionSort();
 		int[] maxValue = temp1.SelectionSort(arr);
-		assertArrayEquals(Sortedarr,arr);	  
+		assertArrayEquals(Sortedarr,arr);
+		  
 		}
 
 	
@@ -52,19 +53,47 @@ public class testSelectionSort  {
 		Sortedarr[4] = -10;
 		
 		SelectionSort temp1 = new SelectionSort();
-		int[] maxValue = temp1.SelectionSort(arr);
+		int[] maxValue = temp1.largest(arr);
 		assert maxValue == -2;
 		}
 	
 	public void testMixes(){        
+		int[] arr = new int[5];
+		arr[0] = -8;
+		arr[1] = 9;
+		arr[2] = -7;
+		arr[3] = 10;
+		arr[4] = -2;
 		
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = -2;
+		Sortedarr[1] = 7;
+		Sortedarr[2] = -8;
+		Sortedarr[3] = 9;
+		Sortedarr[4] = -10;
+		
+		SelectionSort temp1 = new SelectionSort();
+		int[] maxValue = temp1.SelectionSort(arr);
+		assertArrayEquals(Sortedarr,arr);
 		}
 	
 	public void testDuplicates(){
+		int[] arr = new int[5];
+		arr[0] = -8;
+		arr[1] = -8;
+		arr[2] = 7;
+		arr[3] = 7;
+		arr[4] = 2;
 		
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = -2;
+		Sortedarr[1] = -2;
+		Sortedarr[2] = -8;
+		Sortedarr[3] = -8;
+		Sortedarr[4] = 10;
+		
+		SelectionSort temp1 = new SelectionSort();
+		int[] maxValue = temp1.SelectionSort(arr);
+		assertArrayEquals(Sortedarr,arr);
 	}
-
-}
-
-}
-
+	}
