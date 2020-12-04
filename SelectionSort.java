4bf325de0ad4a0640f3c6a7d5825c1d9e9317507
Aleks <asavich@umich.edu>
2020-12-03 19:33:23 -0800

    init commit

---
+++ SelectionSort.java
@@ -0,0 +1,19 @@
+package Lab10.java;
+public class SelectionSort {
+	private int temp;
+	public SelectionSort() {
+		
+	}
+	public int[] SelectionSort(int[] x) {
+		for (int i = 0; i < x.length; ++i) {
+			for (int j= i+1; j < x.length; ++j) {
+				if (x[i] > x[j]) {
+					temp = x[i];
+					x[i] = x[j];
+					x[j] = temp;
+					}
+				} // end of inner for loop        
+			} // end of outer for loop return x;    
+		return x;
+		}
+}
---
+++ testSlectionSort.java
@@ -0,0 +1,46 @@
+package Lab10.java;
+
+import static org.junit.Assert.*;
+import org.junit.Assert;import org.junit.Test;
+import org.junit.Test;
+
+public class testSelectionSort  {
+	private SelectionSort temp1;
+	@Test
+	public void test() {
+		testPositive();
+		testNegative();
+		testMixes();
+		testDuplicates();
+
+		}
+	public void testPositive(){
+		int[] arr = new int[5];
+		arr[0] = 8;
+		arr[1] = 9;
+		arr[2] = 7;
+		arr[3] = 10;
+		arr[4] = 2;
+		
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = 2;
+		Sortedarr[1] = 7;
+		Sortedarr[2] = 8;
+		Sortedarr[3] = 9;
+		Sortedarr[4] = 10;
+		  
+		}
+
+	
+	public void testNegative(){        
+		
+		}
+	
+	public void testMixes(){        
+		
+		}
+	
+	public void testDuplicates(){
+		
+	}
+}
